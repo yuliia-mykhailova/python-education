d00404ee17d61f5749a173cb00522e8d40912c17
yuliia-mykhailova <82874570+yuliia-mykhailova@users.noreply.github.com>
2021-04-26 23:49:57 +0300

    Rename ex1.sh to linux/task1/ex1.sh

--- ex1.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-curl "api.openweathermap.org/data/2.5/weather?q=Kharkiv&appid=42468d97551107e4669eb9b00e19438f" >> $HOME/weather.json
---
+++ linux/task1/ex1.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+curl "api.openweathermap.org/data/2.5/weather?q=Kharkiv&appid=42468d97551107e4669eb9b00e19438f" >> $HOME/weather.json
